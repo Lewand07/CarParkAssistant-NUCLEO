0e6992c1d91b38a11d57eed9fcb00cfdd812a20f
Wojciech Lewandowski <lw51032@zut.edu.pl>
2025-01-27 21:33:52 +0100

    Adjust SensorsDataMutex scope
    Lock the mutex before reading the data from all sensors.
    
    Signed-off-by: Wojciech Lewandowski <lw51032@zut.edu.pl>

--- User/Tasks/Src/sensors_task.c
+++ User/Tasks/Src/sensors_task.c
@@ -4,11 +4,10 @@ void SensorsTask(void) {
     uint8_t i, row, col;
 
     for (;;) {
-        /* Scan each sensor */
-        for (i = 0; i < SENSORS_NUMBER; ++i) {
-            if (SENSOR_OK == Sensor_ReadData(&sensors[i], &sensors_data[i])) {
-                /* Lock mutex before printing or processing data */
-                if (osMutexAcquire(SensorsDataMutexHandle, osWaitForever) == osOK) {
+        if (osOK == osMutexAcquire(SensorsDataMutexHandle, osWaitForever)) {
+            // Read data from all sensors
+            for (i = 0; i < SENSORS_NUMBER; ++i) {
+                if (SENSOR_OK == Sensor_ReadData(&sensors[i], &sensors_data[i])) {
                     printf("Sensor[%d] Data:\n\r", i);
                     for (row = 0; row < sensors_data[i].rows; row++) {
                         for (col = 0; col < sensors_data[i].cols; col++) {
@@ -16,16 +15,16 @@ void SensorsTask(void) {
                         }
                         printf("\n\r");
                     }
-                    osMutexRelease(SensorsDataMutexHandle);  // Unlock mutex
                 } else {
-                    printf("Failed to acquire mutex for Sensor[%d].\n\r", i);
+                    printf("Failed to read data from sensor %d.\n\r", i);
                 }
-            } else {
-                printf("Failed to read data from sensor %d.\n\r", i);
             }
+            osMutexRelease(SensorsDataMutexHandle);  // Unlock mutex
+        } else {
+            printf("Failed to acquire mutex for Sensor[%d].\n\r", i);
         }
 
-        /* Delay to maintain 10Hz frequency */
+        /* Delay to maintain frequency */
         vTaskDelay(pdMS_TO_TICKS(SENSOR_SCAN_DELAY_MS));
     }
 }
